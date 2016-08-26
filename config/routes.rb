180654a1031526f26d0ee42f644ff982bf8c44e8
ozPop <ozziepopovas@gmail.com>
2016-08-26 11:32:00 -0400

    add code to pass tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
+  get 'hello_world', to: 'static#hello_world'
+
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
